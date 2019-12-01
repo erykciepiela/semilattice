6eb899f5a495a2972cbd6fb27bd14285ad923ead
erykciepiela <eryk.ciepiela.dev@gmail.com>
2019-12-01 21:30:29 +0100

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,61 @@
 module Main where
 
 import Semilattice
+import Data.Map as M
 import Data.Set as S
-import qualified Data.Map as M
+import Data.Map.Append
 import Data.Semigroup
 
 -- Can we have a semilattice container where
 -- a) container location is changing
 -- b) container content is changing
-data Container p = Container {
-    containerPath :: SSet Location,
-    containerPositionContent :: SMap (p, String) (Max Int),
+-- ContainerS stands for Container semilattice
+data ContainerS p = ContainerS {
+    containerPath :: S.Set Location,
+    containerPositionContent :: AppendMap (p, String) (Max Int),
     containerLining :: Max Bool
-} deriving (Show, Eq)
+} deriving Show
 
-instance Ord p => Semigroup (Container p) where
-    (Container p1 c1 l1) <> (Container p2 c2 l2) = Container (p1 <> p2) (c1 <> c2) (l1 <> l2)
+instance Ord p => Semigroup (ContainerS p) where
+    (ContainerS p1 c1 l1) <> (ContainerS p2 c2 l2) = ContainerS (p1 <> p2) (c1 <> c2) (l1 <> l2)
 
-instance (Ord p, Eq p) => Semilattice (Container p)
+instance Ord p => Semilattice (ContainerS p)
 
 data Location = LA | LB | LC deriving (Show, Eq, Ord)
 
 -- events/goals
-location :: Ord p => Location -> Container p
-location l = Container (SSet (S.singleton l)) mempty mempty
+location :: Ord p => Location -> ContainerS p
+location l = ContainerS (S.singleton l) mempty mempty
 
-content :: p -> String -> Int -> Container p
-content p s i = Container mempty (SMap (M.singleton (p, s) (Max i))) mempty
+content :: p -> String -> Int -> ContainerS p
+content p s i = ContainerS mempty (AppendMap (M.singleton (p, s) (Max i))) mempty
 
-lining :: Ord p => Container p
-lining = Container mempty mempty (Max True)
+lining :: Ord p => ContainerS p
+lining = ContainerS mempty mempty (Max True)
 
 main :: IO ()
 main = do
     let state = lining <> lining <> location LB <> content 1 "coke" 3 <> content 1 "coke" 3 <> content 2 "fanta" 6 <> location LA <> content 3 "pepsi" 7 <> lining <> content 1 "coke" 5
     let goal = lining <> content 1 "coke" 5 <> content 3 "pepsi" 7 <> content 2 "fanta" 6 <> location LB <> location LA
-    print $ leads state (content 2 "fanta" 8) goal
-    print $ isAchieved goal state
     print goal
     print state
+
+-- let goal' = Con {
+--     conLocation = Just LB,
+--     conPositionContent = M.fromList [((1, "coke"), 5)]
+-- }
+-- print $ leads state (content 2 "fanta" 8) goal
+-- print $ isAchieved goal state
+
+-- data Con p = Con {
+--     conLocation :: Maybe Location,
+--     conPositionContent :: M.Map (p, String) Int,
+--     conLining :: Bool
+-- }
+
+-- con2con :: ContainerS p -> Con p
+-- con2con c = Con {
+--     conLocation = S.lookupMax (sset (containerPath c)),
+--     conPositionContent = getMax <$> smap (containerPositionContent c),
+--     conLining = getMax (containerLining c)
+-- }
--- src/Semilattice.hs
+++ src/Semilattice.hs
@@ -1,54 +1,30 @@
 module Semilattice where
 
-import qualified Data.Map as M
-import qualified Data.Set as S
+import Data.Set
 import Data.Semigroup
+import Data.Map.Append
+import Data.List
 
--- | <> denotes "merging" knowledge
--- | commutativity saves us from out of order messages problem
--- | idempotence saves us from exactly-once delivery guarantee problem
-class (Eq s, Semigroup s) => Semilattice s where
-    -- a <> b = b <> a - commutativity
-    -- a <> a = a - idempotence
+class Semigroup s => Semilattice s where
+    -- a <> b = b <> a - commutativity, saves us from out of order messages problem
+    -- a <> a = a - idempotence, saves us from exactly-once delivery guarantee problem
 
-isAchieved :: Semilattice s => s -> s -> Bool
-isAchieved goal s = s <> goal == s
+merge :: Semilattice s => s -> s -> s
+merge = (<>)
 
-leads :: Semilattice s => s -> s -> s -> Bool
-leads source step target = source <> step <> target == target
+instance (Ord k, Semilattice v) => Semilattice (AppendMap k v)
 
--- | Wrapper over map that instantiates semilattice
-newtype SMap k v = SMap { smap :: M.Map k v}
+instance Ord a => Semilattice (Set a)
 
-deriving instance (Show k, Show v) => Show (SMap k v)
-deriving instance (Eq k, Eq v) => Eq (SMap k v)
+instance (Ord a, Bounded a) => Semilattice (Max a)
 
-instance (Ord k, Semigroup v) => Semigroup (SMap k v) where
-    mm1 <> mm2 = SMap $ M.unionWith (<>) (smap mm1) (smap mm2)
+instance Semilattice All
 
-instance (Ord k, Semigroup v) => Monoid (SMap k v) where
-    mempty = SMap mempty 
+instance Semilattice Any
 
-instance (Ord k, Eq k, Semilattice v) => Semilattice (SMap k v)
+newtype SList a = SList { slist :: [a] } 
 
--- | Wrapper over set that instantiates samilattice
-data SSet a = SSet { sset :: S.Set a}
+instance Semigroup a => Semigroup (SList a) where
+    l1 <> l2 = SList $ foldl1 (<>) <$> transpose [slist l1, slist l2]
 
-deriving instance Show a => Show (SSet a)
-deriving instance Eq a => Eq (SSet a)
-
-instance Ord a => Semigroup (SSet a) where
-    s1 <> s2 = SSet (sset s1 `S.union` sset s2)
-
-instance Ord a => Monoid (SSet a) where
-    mempty = SSet S.empty 
-
-instance Ord a => Semilattice (SSet a)
-
--- |
-instance (Eq a, Ord a, Bounded a) => Semilattice (Max a)
-
--- |
-data SBool = SBool {
-    Maybe 
-}
+instance Semilattice a => Semilattice (SList a) where
