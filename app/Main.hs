4b15136c386512855c3dd40bbdc09374425751c5
erykciepiela <eryk.ciepiela.dev@gmail.com>
2020-01-04 10:36:39 +0100

    propagators

--- app/Main.hs
+++ app/Main.hs
@@ -10,22 +10,20 @@ type PickId = String
 type SkuId = String
 type Qty = Int
 type Batch = (String, SkuId)
-type DTId = String
 type LPN = String
+type DTLogicalId = String
 
 type Bag = S.Map Batch (S.Max Qty)
 
 bag :: PickId -> SkuId -> Qty -> Bag
 bag pickId skuId qty = S.map (pickId, skuId) (S.max qty)
 
-bagContent :: Bag -> Map SkuId Qty
-bagContent b = mapKeysWith (+) snd $ getMax <$> unAppendMap b
-
-type Bag' = S.Map SkuId (S.Max Qty)
+--
+type LogicalBag = S.Map SkuId (S.Max Qty)
 
 -- propagator
-bagContent' :: Bag -> Bag'
-bagContent' b = AppendMap $ mapKeysWith (\max1 max2 -> Max $ getMax max1 + getMax max2) snd $ unAppendMap b
+logicalBag :: Bag -> LogicalBag
+logicalBag b = AppendMap $ mapKeysWith (\max1 max2 -> Max $ getMax max1 + getMax max2) snd $ unAppendMap b
 
 
 type DT = (Bag, Bag, Bag)
@@ -35,14 +33,12 @@ dt 0 b = (b, mempty, mempty)
 dt 1 b = (mempty, b, mempty)
 dt 2 b = (mempty, mempty, b)
 
-dtContent :: DT -> (Map SkuId Qty, Map SkuId Qty, Map SkuId Qty)
-dtContent (b1, b2, b3) = (bagContent b1, bagContent b2, bagContent b3)
-
-type DT' = (Bag', Bag', Bag')
+--
+type LogicalDT = (LogicalBag, LogicalBag, LogicalBag)
 
 -- propagator
-dtContent' :: DT -> DT'
-dtContent' (b1, b2, b3) = (bagContent' b1, bagContent' b2, bagContent' b3)
+logicalDT :: DT -> LogicalDT
+logicalDT (b1, b2, b3) = (logicalBag b1, logicalBag b2, logicalBag b3)
 
 type State = (S.Map DTLogicalId (S.Value LPN), S.Map LPN DT)
 
@@ -55,23 +51,15 @@ picking lpn bagId batchId skuId qty = picking' lpn $ dt bagId $ bag batchId skuI
 assigning :: DTLogicalId -> LPN -> State
 assigning dtid lpn = (S.map dtid (S.Value lpn), mempty)
 
-stateContent :: State -> Map DTLogicalId (S.Value (LPN, (Map SkuId Qty, Map SkuId Qty, Map SkuId Qty)))
-stateContent (assignments, p) = (\plpn -> (\lpn -> (lpn, maybe (mempty, mempty, mempty) dtContent (M.lookup lpn (unAppendMap p)))) <$> plpn) <$> unAppendMap assignments -- dtContent <$> unAppendMap p
-
-type State' = S.Map DTLogicalId (S.Value DT')
+--
+type LogicalDTs = S.Map DTLogicalId (S.Value LogicalDT)
 
 -- propagator
-stateContent' :: State -> State'
-stateContent' (assignments, p) = AppendMap $ (\plpn -> (\lpn -> maybe (mempty, mempty, mempty) dtContent' (M.lookup lpn (unAppendMap p))) <$> plpn) <$> unAppendMap assignments
-
-data Pick = Pick LPN Int PickId SkuId Qty
-
-type F = (S.Promise DTId, S.Promise DTId)
-
-type DTLogicalId = String
+logicalDTs :: State -> LogicalDTs
+logicalDTs (assignments, p) = AppendMap $ (\plpn -> (\lpn -> maybe (mempty, mempty, mempty) logicalDT (M.lookup lpn (unAppendMap p))) <$> plpn) <$> unAppendMap assignments
 
 main :: IO ()
-main = print $ stateContent' $ mconcat [
+main = print $ logicalDTs $ mconcat [
     assigning "1" "123", 
     picking "123" 0 "1" "apple" 3, 
     picking "123" 1 "2" "banana" 4, 
