600195da92b3e488548d4e1308d701841cc61595
erykciepiela <eryk.ciepiela.dev@gmail.com>
2020-01-04 23:07:27 +0100

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import qualified JoinSemilattice as S
+import JoinSemilattice
 import Data.Map as M
 import Data.Map.Append
 import Data.Semigroup
@@ -10,23 +10,22 @@ import Data.Set as Set
 type PickId = String
 type SkuId = String
 type Qty = Int
-type Batch = (String, SkuId)
 type LPN = String
 type LogicalDTId = String
 type BagId = Int
 
 -- join semilattices
-type PhysicalBag = AppendMap Batch (S.Increasing Qty)
+type PhysicalBag = AppendMap (PickId, SkuId) (Increasing Qty)
 
 type PhysicalDT = (PhysicalBag, PhysicalBag, PhysicalBag)
 
 type PhysicalDTs = AppendMap LPN PhysicalDT
 
-type DTAssignment = AppendMap LogicalDTId (S.Same LPN)
+type DTAssignment = AppendMap LogicalDTId (Same LPN)
 
 type PhysicalState = (DTAssignment, PhysicalDTs)
 
-type LogicalBag = AppendMap SkuId (S.Increasing Qty)
+type LogicalBag = AppendMap SkuId (Increasing Qty)
 
 type LogicalDT = (LogicalBag, LogicalBag, LogicalBag)
 
@@ -34,16 +33,16 @@ type LogicalState = AppendMap LogicalDTId LogicalDT
 
 -- propagators
 physicalBag :: PickId -> SkuId -> Qty -> PhysicalBag
-physicalBag pickId skuId qty = AppendMap $ M.singleton  (pickId, skuId) (S.Increasing qty)
+physicalBag pickId skuId qty = AppendMap $ M.singleton  (pickId, skuId) (Increasing qty)
 
 dtAssignment :: LogicalDTId -> LPN -> DTAssignment
-dtAssignment dtid lpn = AppendMap $ M.singleton dtid (S.Unambiguous lpn)
+dtAssignment dtid lpn = AppendMap $ M.singleton dtid (Unambiguous lpn)
 
 logicalBag :: SkuId -> Qty -> LogicalBag
-logicalBag skuId qty = AppendMap $ M.singleton skuId (S.Increasing qty)
+logicalBag skuId qty = AppendMap $ M.singleton skuId (Increasing qty)
 
 physicalToLogicalBag :: PhysicalBag -> LogicalBag
-physicalToLogicalBag b = AppendMap $ mapKeysWith (\max1 max2 -> S.Increasing $ S.increasing max1 + S.increasing max2) snd $ unAppendMap b
+physicalToLogicalBag b = AppendMap $ mapKeysWith (\max1 max2 -> Increasing $ increasing max1 + increasing max2) snd $ unAppendMap b
 
 physicalBagToDT :: Int -> PhysicalBag -> PhysicalDT
 physicalBagToDT 0 b = (b, mempty, mempty)
@@ -60,9 +59,9 @@ logicalBagToDT 2 b = (mempty, mempty, b)
 
 physicalToLogicalState :: PhysicalState -> LogicalState
 physicalToLogicalState (assignments, p) = AppendMap $ (\slpn -> case slpn of
-    S.Unknown -> mempty
-    S.Ambiguous _ -> mempty 
-    (S.Unambiguous lpn) -> maybe mempty physicalTologicalDT (M.lookup lpn (unAppendMap p))) <$> (unAppendMap assignments)
+    Unknown -> mempty
+    Ambiguous _ -> mempty 
+    (Unambiguous lpn) -> maybe mempty physicalTologicalDT (M.lookup lpn (unAppendMap p))) <$> (unAppendMap assignments)
 
 physicalDTtoState :: LPN -> PhysicalDT -> PhysicalState
 physicalDTtoState lpn dt = (mempty, AppendMap $ M.singleton lpn dt)
@@ -82,8 +81,8 @@ main = do
     let actualPhysical = mconcat [ dtAssignment' "1" "123", physicalPick "123" 0 "1" "apple" 3, physicalPick "123" 1 "2" "banana" 4, physicalPick "123" 0 "3" "coconut" 1, physicalPick "123" 0 "4" "coconut" 2, physicalPick "123" 2 "5" "donut" 5, physicalPick "123" 2 "5" "donut" 5, dtAssignment' "2" "444", physicalPick "444" 0 "6" "cucumber" 7]
     let actualLogical = physicalToLogicalState actualPhysical
     let expectedLogic = mconcat [logicalPick "1" 0 "apple" 3, logicalPick "1" 1 "banana" 4]
-    print $ actualLogical S.+> expectedLogic -- True
-    print $ actualLogical S.<+ expectedLogic -- False 
+    print $ actualLogical +> expectedLogic -- True
+    print $ actualLogical <+ expectedLogic -- False 
         where
             physicalPick :: LPN -> BagId -> PickId -> SkuId -> Qty -> PhysicalState
             physicalPick lpn bagId pickId skuId = physicalDTtoState lpn . physicalBagToDT bagId . physicalBag pickId skuId
