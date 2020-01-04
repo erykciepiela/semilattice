e3bb198b71b0e0fb6fe33f0ba0a1896adedb4875
erykciepiela <eryk.ciepiela.dev@gmail.com>
2020-01-04 10:29:33 +0100

    State'

--- app/Main.hs
+++ app/Main.hs
@@ -21,6 +21,13 @@ bag pickId skuId qty = S.map (pickId, skuId) (S.max qty)
 bagContent :: Bag -> Map SkuId Qty
 bagContent b = mapKeysWith (+) snd $ getMax <$> unAppendMap b
 
+type Bag' = S.Map SkuId (S.Max Qty)
+
+-- propagator
+bagContent' :: Bag -> Bag'
+bagContent' b = AppendMap $ mapKeysWith (\max1 max2 -> Max $ getMax max1 + getMax max2) snd $ unAppendMap b
+
+
 type DT = (Bag, Bag, Bag)
 
 dt :: Int -> Bag -> DT
@@ -30,6 +37,12 @@ dt 2 b = (mempty, mempty, b)
 
 dtContent :: DT -> (Map SkuId Qty, Map SkuId Qty, Map SkuId Qty)
 dtContent (b1, b2, b3) = (bagContent b1, bagContent b2, bagContent b3)
+
+type DT' = (Bag', Bag', Bag')
+
+-- propagator
+dtContent' :: DT -> DT'
+dtContent' (b1, b2, b3) = (bagContent' b1, bagContent' b2, bagContent' b3)
 
 type State = (S.Map DTLogicalId (S.Value LPN), S.Map LPN DT)
 
@@ -45,6 +58,12 @@ assigning dtid lpn = (S.map dtid (S.Value lpn), mempty)
 stateContent :: State -> Map DTLogicalId (S.Value (LPN, (Map SkuId Qty, Map SkuId Qty, Map SkuId Qty)))
 stateContent (assignments, p) = (\plpn -> (\lpn -> (lpn, maybe (mempty, mempty, mempty) dtContent (M.lookup lpn (unAppendMap p)))) <$> plpn) <$> unAppendMap assignments -- dtContent <$> unAppendMap p
 
+type State' = S.Map DTLogicalId (S.Value DT')
+
+-- propagator
+stateContent' :: State -> State'
+stateContent' (assignments, p) = AppendMap $ (\plpn -> (\lpn -> maybe (mempty, mempty, mempty) dtContent' (M.lookup lpn (unAppendMap p))) <$> plpn) <$> unAppendMap assignments
+
 data Pick = Pick LPN Int PickId SkuId Qty
 
 type F = (S.Promise DTId, S.Promise DTId)
@@ -52,7 +71,7 @@ type F = (S.Promise DTId, S.Promise DTId)
 type DTLogicalId = String
 
 main :: IO ()
-main = print $ stateContent $ mconcat [
+main = print $ stateContent' $ mconcat [
     assigning "1" "123", 
     picking "123" 0 "1" "apple" 3, 
     picking "123" 1 "2" "banana" 4, 
