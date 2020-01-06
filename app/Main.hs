1bffd8b8af19159c520af2e4ba16e06aabb53640
erykciepiela <eryk.ciepiela.dev@gmail.com>
2020-01-06 15:02:43 +0100

    simplification

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,8 @@ module Main where
 
 import Semilattice
 import Data.Map as M
+import Data.Maybe
 
-type PickId = String
 type SkuId = String
 type Qty = Int
 type LPN = String
@@ -11,7 +11,7 @@ type LogicalDTId = String
 type BagId = Int
 
 -- bounded join semilattices - objects
-type PhysicalBag = Map (PickId, SkuId) (Increasing Qty)
+type PhysicalBag = Map SkuId (Increasing Qty)
 
 type PhysicalDT = (PhysicalBag, PhysicalBag, PhysicalBag)
 
@@ -21,49 +21,31 @@ type DTAssignment = Map LogicalDTId (Same LPN)
 
 type PhysicalState = (DTAssignment, PhysicalDTs)
 
-type LogicalBag = Map SkuId (Increasing Qty)
-
-type LogicalDT = (LogicalBag, LogicalBag, LogicalBag)
-
-type LogicalState = Map LogicalDTId LogicalDT
+type LogicalState = Map LogicalDTId PhysicalDT
 
 -- homomorphisms - morphisms
-physicalBag :: PickId -> SkuId -> Qty -> PhysicalBag
-physicalBag pickId skuId qty = base ((pickId, skuId), Increasing qty)
+physicalBag :: SkuId -> Qty -> PhysicalBag
+physicalBag skuId qty = base (skuId, Increasing qty)
 
 dtAssignment :: LogicalDTId -> LPN -> DTAssignment
 dtAssignment dtid lpn = base (dtid, Unambiguous lpn)
-
-logicalBag :: SkuId -> Qty -> LogicalBag
-logicalBag skuId qty = base (skuId, Increasing qty)
-
-physicalToLogicalBag :: PhysicalBag -> LogicalBag
-physicalToLogicalBag = propagateMap (propagateIncrease2 (+)) snd
 
 physicalBagToDT :: Int -> PhysicalBag -> PhysicalDT
 physicalBagToDT 0 b = (b, bottom, bottom)
 physicalBagToDT 1 b = (bottom, b, bottom)
 physicalBagToDT 2 b = (bottom, bottom, b)
 
-physicalTologicalDT :: PhysicalDT -> LogicalDT
-physicalTologicalDT (b1, b2, b3) = (physicalToLogicalBag b1, physicalToLogicalBag b2, physicalToLogicalBag b3)
-
-logicalBagToDT :: Int -> LogicalBag -> LogicalDT
-logicalBagToDT 0 b = (b, bottom, bottom)
-logicalBagToDT 1 b = (bottom, b, bottom)
-logicalBagToDT 2 b = (bottom, bottom, b)
-
 physicalToLogicalState :: PhysicalState -> LogicalState
 physicalToLogicalState (assignments, p) = (\slpn -> case slpn of
     Unknown -> bottom
     Ambiguous _ -> bottom 
-    (Unambiguous lpn) -> maybe bottom physicalTologicalDT (M.lookup lpn p)) <$> assignments
+    (Unambiguous lpn) -> fromMaybe bottom (M.lookup lpn p)) <$> assignments
 
 physicalDTtoState :: LPN -> PhysicalDT -> PhysicalState
 physicalDTtoState lpn dt = (bottom, base (lpn, dt))
 
-logicalDTtoState :: LogicalDTId -> LogicalDT -> LogicalState
-logicalDTtoState dtId dt = base (dtId, dt)
+-- logicalDTtoState :: LogicalDTId -> LogicalDT -> LogicalState
+-- logicalDTtoState dtId dt = base (dtId, dt)
 
 dtAssignmentToPhysicalState :: DTAssignment -> PhysicalState
 dtAssignmentToPhysicalState a = (a, bottom)
@@ -71,15 +53,15 @@ dtAssignmentToPhysicalState a = (a, bottom)
 --
 main :: IO ()
 main = do
-    let actualPhysical = mconcat [ dtAssignment' "1" "123", physicalPick "123" 0 "1" "apple" 3, physicalPick "123" 1 "2" "banana" 4, physicalPick "123" 0 "3" "coconut" 1, physicalPick "123" 0 "4" "coconut" 2, physicalPick "123" 2 "5" "donut" 5, physicalPick "123" 2 "5" "donut" 5, dtAssignment' "2" "444", physicalPick "444" 0 "6" "cucumber" 7]
+    let actualPhysical = mconcat [ dtAssignment' "1" "123", physicalPick "123" 0 "apple" 3, physicalPick "123" 1 "banana" 4, physicalPick "123" 0 "coconut" 1, physicalPick "123" 0 "coconut" 2, physicalPick "123" 2 "donut" 5, physicalPick "123" 2 "donut" 5, dtAssignment' "2" "444", physicalPick "444" 0 "cucumber" 7]
     let actualLogical = physicalToLogicalState actualPhysical
     let expectedLogic = mconcat [logicalPick "1" 0 "apple" 3, logicalPick "1" 1 "banana" 4]
     print $ actualLogical +> expectedLogic -- True
     print $ actualLogical <+ expectedLogic -- False 
         where
-            physicalPick :: LPN -> BagId -> PickId -> SkuId -> Qty -> PhysicalState
-            physicalPick lpn bagId pickId skuId = physicalDTtoState lpn . physicalBagToDT bagId . physicalBag pickId skuId
+            physicalPick :: LPN -> BagId -> SkuId -> Qty -> PhysicalState
+            physicalPick lpn bagId skuId = physicalDTtoState lpn . physicalBagToDT bagId . physicalBag skuId
             logicalPick :: LogicalDTId -> BagId -> SkuId -> Qty -> LogicalState
-            logicalPick dtId bagId skuId = logicalDTtoState dtId . logicalBagToDT bagId . logicalBag skuId
+            logicalPick dtId bagId skuId = physicalToLogicalState . physicalDTtoState dtId . physicalBagToDT bagId . physicalBag skuId
             dtAssignment' :: LogicalDTId -> LPN -> PhysicalState
             dtAssignment' dtid lpn = dtAssignmentToPhysicalState $ dtAssignment dtid lpn
