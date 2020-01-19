9bac72da3b92bbb66a4669b1caa42623277a3e7c
erykciepiela <eryk.ciepiela.dev@gmail.com>
2020-01-19 12:35:16 +0100

    example

--- app/Main.hs
+++ app/Main.hs
@@ -99,7 +99,7 @@ main = do
                 vanManufactured 0 "V1"       -- 0 has been loaded and it has LPN V1
             ]
     let expectedState = GrowingMap {growingMap = fromList [(0,(Unambiguous "V1",GrowingMap {growingMap = fromList [(0,(Unambiguous "F1",GrowingMap {growingMap = fromList [(0,(Unambiguous "DT1",GrowingMap {growingMap = fromList [(0,GrowingMap {growingMap = fromList [("apple",GrowingMap {growingMap = fromList [(0,Increasing {increasing = 3})]}),("coconut",GrowingMap {growingMap = fromList [(2,Increasing {increasing = 1}),(3,Increasing {increasing = 1})]})]}),(1,GrowingMap {growingMap = fromList [("banana",GrowingMap {growingMap = fromList [(1,Increasing {increasing = 4})]})]}),(2,GrowingMap {growingMap = fromList [("donut",GrowingMap {growingMap = fromList [(4,Increasing {increasing = 5})]})]})]})),(1,(Unambiguous "DT2",GrowingMap {growingMap = fromList [(0,GrowingMap {growingMap = fromList [("donut",GrowingMap {growingMap = fromList [(4,Increasing {increasing = 7})]})]})]}))]}))]}))]}
-    print $ all (`isEventuallyConsistent` expectedState) (L.take 100000 (messedUp (jirelement <$> events)))  -- True
+    print $ all (\es -> (fmap jirelement <$> es) `isEventuallyConsistent` expectedState) (L.take 100000 (messedUp events))  -- True
     print $ (`propagatedHomo` (prop . composeHomo)) (collect events) `ascendsTowards` (GrowingSet {growingSet = S.fromList ["apple","coconut"]})
     print $ decompose expectedState 
     -- print $ all (`isEventuallyConsistent` (GrowingSet {growingSet = S.fromList ["apple","coconut"]})) ((propagateHomo prop . fmap (fmap bjsconcat) <$> (L.take 100000 (messedUp events))))
